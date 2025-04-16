6b6d0c2740997ecc72dd7b38617873813fe43ac0
vikramchavan888 <vikramchavan69ps@gmail.com>
2025-04-16 11:13:50 +0530

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/App/index";
+import App from "./pages/App/index.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
